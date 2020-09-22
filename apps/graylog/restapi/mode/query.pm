92105d4ad7582f97828818f9687454958915fb9e
Sims24 <sbomm@centreon.com>
2020-09-22 10:38:17 +0200

    (draft)graylog-plugin

---
+++ apps/graylog/restapi/mode/query.pm
@@ -0,0 +1,89 @@
+#
+# Copyright 2020 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package apps::graylog::restapi::mode::query;
+
+use base qw(centreon::plugins::templates::counter);
+
+use strict;
+use warnings;
+
+sub set_counters {
+    my ($self, %options) = @_;
+
+    $self->{maps_counters_type} = [
+        { name => 'global', type => 0, skipped_code => { -10 => 1 } },
+    ];
+
+    $self->{maps_counters}->{global} = [
+        { label => 'count', nlabel => 'graylog.query.match.count', set => {
+                key_values => [ { name => 'count' } ],
+                output_template => 'current queue messages : %s',
+                perfdatas => [
+                    { label => 'queue_msg', value => 'queue_messages_absolute', template => '%d',
+                      min => 0 },
+                ],
+            }
+        },
+    ];
+}
+
+sub new {
+    my ($class, %options) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options, force_new_perfdata => 1);
+    bless $self, $class;
+
+    $options{options}->add_options(arguments => {
+        "query:s"       => { name => 'query' },
+        "timeframe:s"   => { name => 'timeframe', default => '300'}
+    });
+    return $self;
+}
+
+sub manage_selection {
+    my ($self, %options) = @_;
+
+    my $result = $options{custom}->query_absolute(query => $self->{option_results}->{query},
+                                                  timeframe => $self->{option_results}->{timeframe});
+    use Data::Dumper; print Dumper($result);
+    $self->{global} = {
+
+    };
+
+
+}
+
+1;
+
+__END__
+
+=head1 MODE
+
+Perform Lucene queries against Graylog API
+
+=over 8
+
+=item B<--warning-count> B<--critical-count>
+
+Threshold on the number of results
+
+=back
+
+=cut
